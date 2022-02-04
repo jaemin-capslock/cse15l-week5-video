1d7d53c655cabfcb2d5a584d45d28ae2278c0c83
Min <jaeminko0420@gmail.com>
2022-02-04 15:10:22 -0800

    initial commit

---
+++ MyMulti.java
@@ -0,0 +1,5 @@
+public class MyMulti{
+    public static int myMulti(int a, int b){
+        return a * b;
+    }
+}
---
+++ MyMultiTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class MyMultiTest {
+    @Test
+    public void TestMulti(){
+        assertEquals(10, MyMulti.myMulti(2, 4));
+    }
+}
